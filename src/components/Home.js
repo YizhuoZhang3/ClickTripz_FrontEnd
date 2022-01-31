a61e722e45a4d03b3f6b7c3907203ce42ff2a17d
Zoe Zhang <zhangyizhuozoe@gmail.com>
2022-01-30 22:10:06 -0500

    styling form

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,13 +24,32 @@ export default function Home(){
         <div className="heading">
           <h2>Company Information</h2>
         </div>
-        <div className="content">
-          <h3 className="comp-info" id="company-name">Company Name</h3>
-          <h3 className="comp-info" id="company-loca">Location</h3>
-          <h3 className="comp-info" id="company-id">Organization ID</h3>
-          <h3 className="comp-info" id="company-phone">Phone</h3>
-          <h3 className="comp-info" id="company-bio">Bio</h3>
-        </div>
+
+        <form className="content">
+          <label className="comp-info" id="company-name" for="name">
+            Company Name
+            <input type="text" id="name" value={data.name} required/>
+          </label>
+      
+          <label className="comp-info" id="company-loca" for="location">
+            Location
+            <input type="text" id="location" value={"New York"}/>
+          </label>
+           
+          <label className="comp-info" id="company-id" for="org-id">Organization ID
+          <input type="text" id="org-id" value={data.organizationId}/>
+          </label>
+          
+          <label className="comp-info" id="company-phone" for="phone">Phone
+          <input type="tel" id="phone" value={"917-232-2323"}/>
+          </label>
+          
+          <label className="comp-info" id="company-bio" for="bio">Bio
+          <input type="text" id="bio" value={"ClickTripz is a travel technology company that builds innovative software solutions to serve the needs of suppliers, publishers, advertisers and travelers. From marketplaces that connect the industry through proprietary ad-serving technology, to cloud-based tools that help hoteliers put heads in beds-- ClickTripz has solutions that drive revenue atscale. Travelers benefit from the transparent and efficient pricing facilitated by the platform, while suppliers, publishers and advertisers all benefit from the enhanced ability to acquire and monetize qualified traffic using precise targeting and granular bidding. Founded in 2010 and with offices in California, Washington DC and London, the platform currently comprises thousands of active campaigns from worldwide partners, spanning 31 different languages and addressing tens of millions of travel queries per month."}/>
+          </label>
+          <button>SAVE</button>
+        </form>
+        
       </div>
     </div>
   )
