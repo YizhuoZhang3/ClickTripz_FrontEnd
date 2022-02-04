60b0b25dd58cd47d2cb7656228330eefc7f8c71f
Zoe Zhang <zhangyizhuozoe@gmail.com>
2022-02-03 19:01:27 -0500

    bug for toggle button

--- src/components/Site2.js
+++ src/components/Site2.js
@@ -17,6 +17,7 @@ export default function Site2({data, handleSubmit, onToggle, handleSite2InfoChan
           handleChange={handleSite2InfoChange}
           handleSubmit={handleSubmit}
           onToggle={onToggle}
+          siteNum={2}
         />
     </div>
   )
